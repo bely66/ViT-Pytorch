052bfea0bab565c9b63cb0f091cd03b55cebae40
mohamed nabil <medo661998@gmail.com>
2023-04-19 20:36:35 +0200

    add_mlp

--- modules.py
+++ modules.py
@@ -106,3 +106,48 @@ class Attention(nn.Module):
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
+    
+
+class MLP(nn.Module):
+    """ 
+    MLP Module
+    Parameters
+    ----------
+    in_features : int
+        Number of input features
+    hidden_features : int
+        Number of features in the hidden layer
+    out_features : int
+        Number of output features
+    act_layer : nn.Module
+        Activation layer
+    drop : float
+        Dropout ratio
+    """
+
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        '''
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input feature map with shape (B, N, C)
+        Returns
+        -------
+        x : torch.Tensor
+            MLP feature map with shape (B, N, C)
+        '''
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
